1c562ce2bf98f50df1972c560d55ddb910a1cfa5
Peter Miklos <petermiklos@Peters-MacBook-Pro.local>
2016-08-31 16:20:44 +0100

    Ex #21 completed

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Peter'
-    fill_in :player_2_name, with: 'Stephan'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Peter vs. Stephan'
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 
 feature 'see player hits' do
   scenario 'see player 2 hit points' do
-    visit('/')
-    fill_in :player_1_name, with: 'Peter'
-    fill_in :player_2_name, with: 'Stephan'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content "Stephan 200 points"
   end
 end
