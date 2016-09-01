8332b023ff5a960923a3e10457676f8333ed9821
matt vickers <matt.vickers@sky.com>
2016-09-01 12:22:12 +0100

    add test helpers

--- battle/spec/features/hit_points_spec.rb
+++ battle/spec/features/hit_points_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 
 feature 'shows hitpoints of players' do
   scenario 'display hitpoints of players' do
-    visit('/')
-    fill_in :player_1_name, with: "Matt"
-    fill_in :player_2_name, with: "Alfie"
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_content "Matt: 100, Alfie: 100"
   end
 end
