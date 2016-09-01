5c50e7cc978b408e1d54eae0bdb2d9cda5f3a274
ercekal <ercekalabalikoglu@gmail.com>
2016-09-01 11:30:23 +0100

    #20 was done

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -9,5 +9,15 @@ feature 'Enter names' do
     click_button 'BATTLE!'
     expect(page).to have_content 'Abi vs. Erce'
   end
+end
+
+feature 'hit points' do
+  scenario 'shows the hit point of player 2' do
+    visit('/')
+    fill_in :player_1_name, with: 'Peter'
+    fill_in :player_2_name, with: 'Erce'
+    click_button 'BATTLE!'
+    expect(page).to have_content "Erce's hitpoint is 200"
+end
 
 end
