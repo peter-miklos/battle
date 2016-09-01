47d08bfffff0ad15bcdd2f399772854fe951a8bb
Peter Miklos <petermiklos@Peters-MacBook-Pro.local>
2016-09-01 16:14:25 +0100

    Ex #26 completed

--- app.rb
+++ app.rb
@@ -7,30 +7,28 @@ class Battle < Sinatra::Base
   enable :sessions
 
   get '/' do
-    $game = Game.new
     erb :index
   end
 
   post '/names' do
-    $player1 = Player.new(params[:player_1_name])
-    $player2 = Player.new(params[:player_2_name])
+    player1 = Player.new(params[:player_1_name])
+    player2 = Player.new(params[:player_2_name])
+    $game = Game.new(player1, player2)
     redirect '/play'
   end
 
   get '/play' do
-    @player_1 = $player1.name
-    @player_2 = $player2.name
-    @hitpoints1 = $player1.points
-    @hitpoints2 = $player2.points
+    @game = $game
     erb :play
   end
 
   get '/attack' do
-    @player_1 = $player1.name
-    @player_2 = $player2.name
+    @game = $game
+    # @player_1 = $game.player1.name
+    # @player_2 = $game.player2.name
     # $player1.attack($player2)
-    $game.attack($player2)
-    @hitpoints2 = $player2.points
+    $game.attack($game.player2)
+    # @hitpoints2 = $game.player2.points
     erb :attack
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,19 @@
 require 'game'
 
 describe Game do
-  subject(:game) {described_class.new}
   let(:player1) {double :player1}
   let(:player2) {double :player2}
+  subject(:game) {described_class.new(player1, player2)}
+
+  context "#initialize" do
+    it "stores player1 object" do
+      expect(game.player1).to eq player1
+    end
+
+    it "stores player2 object" do
+      expect(game.player2).to eq player2
+    end
+  end
 
   context '#attack' do
     it "calls the #receive_damage method on the attacked player object" do
