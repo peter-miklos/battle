43ac2975a6b81795e1726e3de4389ca513fd1b00
matt vickers <matt.vickers@sky.com>
2016-09-02 17:48:19 +0100

    refactor class variables removing global variables

--- battle/app.rb
+++ battle/app.rb
@@ -10,26 +10,27 @@ class Battle < Sinatra::Base
 
 enable :sessions
 
+  before do
+    @game = Game.instance
+  end
+
   get '/play' do
-    @game = $game
     erb :play
   end
 
   post '/names' do
     player_1 = Player.new(params[:player_1])
     player_2 = Player.new(params[:player_2])
-    $game = Game.new(player_1, player_2)
+    Game.create(player_1, player_2)
     redirect '/play'
   end
 
   get '/attack' do
-    @game = $game
     @game.attack(@game.attacked_player)
     erb :attack
   end
 
   get '/switch_turns' do
-    @game = $game
     @game.switch_turns
     redirect '/play'
   end
--- battle/lib/game.rb
+++ battle/lib/game.rb
@@ -7,6 +7,14 @@ class Game
     @attacker = player_1
   end
 
+  def self.create(player_1, player_2)
+    @game = Game.new(player_1, player_2)
+  end
+
+  def self.instance
+    @game
+  end
+
   def player_1
     @players.first
   end
